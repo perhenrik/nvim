30541ce1556f7bbc0bef6034d65349a4e4783e48
Per Henrik Johansen <perhenrik@aera.id>
2023-12-29 14:58:03 +0100

    Added support for PlanUml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -1,8 +1,29 @@
 return {
-    "javiorfo/nvim-soil",
-    lazy = true,
-    ft = "puml",
-    config = function()
-        -- If you want to change default configurations
-    end,
+    -- plantuml syntax
+    {
+        "aklt/plantuml-syntax",
+        ft = { "puml", "plantuml", "markdown" },
+    },
+
+    -- plantuml preview in browser
+    {
+        "weirongxu/plantuml-previewer.vim",
+        ft = { "puml", "plantuml" },
+        dependencies = { "tyru/open-browser.vim" },
+        cmd = {
+            "PlantumlToggle",
+            "PlantumlOpen",
+            "PlantumlStart",
+            "PlantumlStop",
+            "PlantumlSave",
+        },
+        init = function()
+            vim.g["plantuml_previewer#plantuml_jar_path"] = (vim.env.XDG_LIB_HOME or vim.env.HOME .. "/.config")
+                .. "/java/plantuml.jar"
+            -- stylua: ignore
+            --     require("utils").on_ft("plantuml", function(event)
+            --       vim.keymap.set("n", "<leader>cp", "<cmd>PlantumlToggle<cr>", { desc = "Live Preview (Plantuml)", buffer = event.buf })
+            --     end)
+        end,
+    },
 }
